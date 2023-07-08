f9bd4c2b1243d8dec54b9015c943c0b54cf0c677
Amree Zaid <mohd.amree@gmail.com>
2023-07-08 21:18:37 +0800

    Initial Commit

---
+++ petakopi/petakopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  petakopiApp.swift
+//  petakopi
+//
+//  Created by Amree Zaid on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct petakopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
