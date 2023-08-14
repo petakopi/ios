3f7bb8f87184a0613d8010615baf8706d878e7b0
Amree Zaid <mohd.amree@gmail.com>
2023-08-14 09:44:59 +0800

    Stop camera from following the puck

--- petakopi/Features/Map/Views/MapView.swift
+++ petakopi/Features/Map/Views/MapView.swift
@@ -18,6 +18,7 @@ struct MapViewWrapper : UIViewControllerRepresentable {
 
     }
 }
+
 class ViewController: UIViewController {
 
     internal var mapView: MapView!
@@ -26,12 +27,11 @@ class ViewController: UIViewController {
         longitude: 101.71174101512028
     )
 
-    private var cameraLocationConsumer: CameraLocationConsumer!
-
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         let option = MapInitOptions(
+            // Can we use the last known location?
             cameraOptions: CameraOptions(center: klcc, zoom: 14.5),
             styleURI: StyleURI.streets
         )
@@ -49,15 +49,9 @@ class ViewController: UIViewController {
 
         mapView.location.options.puckType = .puck2D(configuration)
 
-        cameraLocationConsumer = CameraLocationConsumer(mapView: mapView)
-
         mapView.mapboxMap.onNext(event: .mapLoaded) { [self]_ in
             // Ensure it will set the camera to the current location
             self.locationUpdate(newLocation: mapView.location.latestLocation!)
-
-            // Ensure the camera will follow the puck when we move
-            // TODO: Add option to stop following which is helpful if using the app while driving
-            self.mapView.location.addLocationConsumer(newConsumer: self.cameraLocationConsumer)
         }
     }
 }
@@ -65,21 +59,5 @@ class ViewController: UIViewController {
 extension ViewController: LocationPermissionsDelegate, LocationConsumer {
     func locationUpdate(newLocation: Location) {
         mapView.camera.fly(to: CameraOptions(center: newLocation.coordinate, zoom: 14.0), duration: 5.0)
-    }
-}
-
-// Create class which conforms to LocationConsumer,
-// update the camera's centerCoordinate when a locationUpdate is received
-public class CameraLocationConsumer: LocationConsumer {
-    weak var mapView: MapView?
-
-    init(mapView: MapView) {
-        self.mapView = mapView
-    }
-
-    public func locationUpdate(newLocation: Location) {
-        mapView?.camera.ease(
-            to: CameraOptions(center: newLocation.coordinate, zoom: 15),
-            duration: 1.3)
     }
 }
